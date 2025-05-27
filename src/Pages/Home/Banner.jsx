5ecb78e28a5976ef0b1f16d94396588f73a092e7
rafiahmedrifatt <rafiahmed0471@gmail.com>
2025-05-27 14:01:25 +0600

    added react router and other routing functionality added

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { motion } from "motion/react"
+
+const Banner = () => {
+    return (
+        <motion.div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img
+                    src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                    className="max-w-xl rounded-lg shadow-2xl"
+                />
+                <div>
+                    <motion.h1
+                        initial={{ scale: 0 }}
+                        animate={{ scale: 1, transition: { duration: 2 } }}
+                        className="text-5xl font-bold">Box Office News!</motion.h1>
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </motion.div>
+    );
+};
+
+export default Banner;
